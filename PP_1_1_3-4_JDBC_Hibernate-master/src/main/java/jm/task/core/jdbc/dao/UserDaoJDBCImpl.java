ea40d211ad3589d6784f54113a97bd37b12edb3c
Acc3l3rat0r <kraje.robert@gmail.com>
2022-08-15 18:24:10 +0300

    1.1

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,9 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,15 +12,43 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS kata.users (\n" +
+                "  id INT NOT NULL AUTO_INCREMENT,\n" +
+                "  name VARCHAR(45) NOT NULL,\n" +
+                "  lastName VARCHAR(45) NOT NULL,\n" +
+                "  age INT NOT NULL,\n" +
+                "  PRIMARY KEY (id));";
+        try (Connection dbConnection = Util.getDBConnection();
+             Statement statement = dbConnection.createStatement()) {
+            statement.execute(createTableSQL);
+            System.out.println("Table created");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void dropUsersTable() {
+        String dropTable = "DROP TABLE IF EXISTS `kata`.`users`;";
+        try (Connection dbConnection = Util.getDBConnection();
+             Statement statement = dbConnection.createStatement()) {
 
+            statement.execute(dropTable);
+            System.out.println("Table dropped");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String createUserSQL = String.format("INSERT INTO kata.users (name, lastname, age) VALUES (\"%s\", \"%s\",\"%s\");",name,lastName,age);
+        try (Connection dbConnection = Util.getDBConnection();
+             Statement statement = dbConnection.createStatement()) {
 
+            statement.execute(createUserSQL);
+            System.out.println("User successfully added");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void removeUserById(long id) {
--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/mysql";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
+    public static Connection getDBConnection() {
+        Connection dbConnection = null;
+        try{
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+            System.out.println("Connection successful");
+            return dbConnection;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return dbConnection;
+    }
 }
